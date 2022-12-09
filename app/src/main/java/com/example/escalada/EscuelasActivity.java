2a41dfd82efde05d13c6ca86ccf3977f9d87b805
JesGonzL <jesusgl99@outlook.com>
2022-12-09 23:08:11 +0100

    Comenzando la activity_escuelas.xml

--- app/src/main/java/com/example/escalada/EscuelasActivity.java
+++ app/src/main/java/com/example/escalada/EscuelasActivity.java
@@ -4,15 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class EscuelasActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        // Instanciar base de datos
-        ViasDB viasDB = new ViasDB(this);
-
+        setContentView(R.layout.activity_escuelas);
     }
 }
